f40ea93f3b13bbaa0181a812123bd68014fadab2
Jason Wu <jason.wu2153@gmail.com>
2025-09-07 12:41:04 -0700

    checkpoint

--- src/parser.ts
+++ src/parser.ts
@@ -10,3 +10,16 @@ const MarionetteConfig = type({
 });
 
 export type MarionetteConfig = typeof MarionetteConfig.infer;
+
+async function parseMarionetteConfig(path: string): MarionetteConfig {
+	const file = Bun.file(path);
+	const contents = await file.text();
+
+	// TODO toml parser
+
+	const result = MarionetteConfig(contents);
+	if (result instanceof type.errors) {
+		throw new Error("Failed to parse marionette config");
+	}
+	return result;
+}
