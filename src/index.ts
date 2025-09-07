b2eacd6f4fc0a489f179f4a5f196a32939fb1707
wu-json <jason.wu2153@gmail.com>
2025-09-07 10:13:27 -0700

    feat: version

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { run } from "@drizzle-team/brocli";
 
 import { commands } from "./commands";
 
-run(commands, { name: "marionette", description: "Marionette CLI" });
+run(commands, {
+	name: "marionette",
+	description: "Marionette CLI",
+	version: "0.0.0",
+});
