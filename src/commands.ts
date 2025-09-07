20c4fe4c9772d68990e36520de14f66c1f55693f
wu-json <jason.wu2153@gmail.com>
2025-09-07 10:51:35 -0700

    feat: update

--- src/commands.ts
+++ src/commands.ts
@@ -2,6 +2,7 @@ import { command, type Command, string } from "@drizzle-team/brocli";
 
 const startCmd = command({
 	name: "start",
+	desc: "run marionette in your terminal",
 	options: {
 		path: string().default("./marionette.toml"),
 	},
