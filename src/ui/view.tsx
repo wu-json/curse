2757e7fe3b0cc89e06f7a7241aaf13a7f4ae45dd
wu-json <jason.wu2153@gmail.com>
2025-09-27 17:08:50 -0700

    feat: update

--- src/ui/view.tsx
+++ src/ui/view.tsx
@@ -41,7 +41,7 @@ function View(props: { config: CurseConfig }) {
 			</Box>
 			<Box flexDirection="row">
 				<Text color={Colors.primary}>Config: </Text>
-				<Text>{props.config.filename}</Text>
+				<Text>{props.config.fileName}</Text>
 			</Box>
 			{(() => {
 				switch (page) {
